2fa347d7f547da8817855d38fe7d9a9249d12e59
Ben Guo <ben@venmo.com>
2015-03-20 08:28:07 -0400

    .

--- RefreshToDeath/ViewController.swift
+++ RefreshToDeath/ViewController.swift
@@ -12,43 +12,41 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var webview1: UIWebView!
     @IBOutlet weak var webview2: UIWebView!
+    @IBOutlet weak var label: UILabel!
 
     var timer : NSTimer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         NSURLCache.sharedURLCache().removeAllCachedResponses()
+
         let url1: NSURL = NSURL(string: "http://netsadness.net")!
         let request1 = NSURLRequest(URL: url1)
-        let url2: NSURL = NSURL(string: "http://netsadness.net/wayne")!
-        let request2 = NSURLRequest(URL: url1)
+        let url2: NSURL = NSURL(string: "http://benzguo.com")!
+        let request2 = NSURLRequest(URL: url2)
 
         webview1.loadRequest(request1)
         webview1.mediaPlaybackRequiresUserAction = false
         webview2.loadRequest(request2)
         webview2.mediaPlaybackRequiresUserAction = false
 
-
+        timer = NSTimer.scheduledTimerWithTimeInterval(40, target: self, selector: "reload", userInfo: nil, repeats: true)
     }
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        timer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: "reload", userInfo: nil, repeats: true)
-    }
-
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-        timer.map{$0.invalidate()}
+    override func didReceiveMemoryWarning() {
+        label.text = "memory warning"
+        NSURLCache.sharedURLCache().removeAllCachedResponses()
     }
 
     func reload() {
-        let r = arc4random_uniform(1)
+        let r = arc4random_uniform(2)
         print(r)
         if r == 0 {
-        webview1.reload()
+            webview1.reload()
         }
         else if r == 1 {
-         webview2.reload()
+            webview2.reload()
         }
 
     }
